97650c9745c3796b295a51818ad0317a6e782529
Daniel Lingan <dalc.2506>
2021-10-04 22:11:39 -0500

    [ UPDATE ] Implementation of redux, fetching data and embedding of global stats

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'e724cf9f18mshd7015193168a1bfp16c2c2jsn8a1c66f1b913'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins')
+    })
+  })
+});
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;
